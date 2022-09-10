1c36b6c199fae2a4b591ffe7dced29c511edf7b9
Kaymohh <thrilltim@gmail.com>
2022-09-09 21:40:09 -0700

    Enough

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    *
+ *     * Return: Always 0 (Sucess)
+*/
 
 int main(void)
 {
-	char low;
+char c;
 
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
