b51ac5776faa8dc23ab8001f15053233ea264904
Kaymohh <thrilltim@gmail.com>
2022-09-15 14:02:44 -0700

    COrrection

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,17 +3,9 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
-int _isdigit(int c);
-int _isupper(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif
